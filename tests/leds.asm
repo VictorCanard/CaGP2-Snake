d47f96e67e1d1ca0670a871116dfd0f9ae2c7042
Victor Canard-Duchêne <victor.canardduchene@epfl.ch>
2021-11-02 16:35:17 +0100

    did tests. set pixel and clear do not work.

---
+++ tests/leds.asm
@@ -0,0 +1,69 @@
+	call clear_leds
+
+	addi a0, zero, 1
+	addi a1, zero, 1
+
+	call set_pixel
+
+	addi a0, zero, 2
+	addi a1, zero, 1
+
+	call set_pixel
+
+	addi a0, zero, 2
+	addi a1, zero, 2
+
+	call set_pixel
+
+	addi a0, zero, 2
+	addi a1, zero, 3
+
+	call set_pixel
+
+	addi a0, zero, 3
+	addi a1, zero, 1
+
+	call set_pixel
+	
+	addi a0, zero, 5
+	addi a1, zero, 1
+
+	call set_pixel
+
+	addi a0, zero, 5
+	addi a1, zero, 2
+
+	call set_pixel
+
+	addi a0, zero, 5
+	addi a1, zero, 3
+
+	call set_pixel
+
+	addi a0, zero, 5
+	addi a1, zero, 4
+
+	call set_pixel
+
+	addi a0, zero, 5
+	addi a1, zero, 5
+
+	call set_pixel
+
+	addi a0, zero, 6
+	addi a1, zero, 1
+
+	call set_pixel
+
+	addi a0, zero, 6
+	addi a1, zero, 3
+
+	call set_pixel
+
+	addi a0, zero, 6
+	addi a1, zero, 5
+
+	call set_pixel
+	
+	
+	call clear_leds
